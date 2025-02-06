a559f9dd96a041d03cfd220c86bbaa6ff4e59e58
jatin-yadav <jatinyadav19oct@gmail.com>
2025-02-07 00:21:58 +0530

    signup endpoint

--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -1,12 +1,37 @@
-import { Router, type Router as ExpressRouter } from "express";
+import {
+  Request,
+  Response,
+  Router,
+  type Router as ExpressRouter,
+} from "express";
 import { userRouter } from "./user";
 import { spaceRouter } from "./space";
 import { adminRouter } from "./admin";
+import { SignupSchema } from "../../types";
+import client from "@repo/db/client";
 
 export const router: ExpressRouter = Router();
 
-router.post("/signup", (req, res) => {
-  res.json({ message: "Signup" });
+router.post("/signup", async (req: Request, res: Response) => {
+  const parseData = SignupSchema.safeParse(req.body);
+  if (!parseData.success) {
+    res.status(400).json({ message: "Invalid data validation failed" });
+    return;
+  }
+
+  try {
+    const user = await client.user.create({
+      data: {
+        username: parseData.data.username,
+        password: parseData.data.password,
+        role: parseData.data.type === "admin" ? "Admin" : "User",
+      },
+    });
+
+    res.json({ userID: user.id });
+  } catch (error) {
+    res.status(400).json({ message: "user already exist" });
+  }
 });
 
 router.post("/signin", (req, res) => {
